c95a343d3cf571dd2cd04409e5267c05acfd33bf
Pratyakshya Mishra <pratyakshya.mishra43@gmail.com>
2026-06-15 18:03:32 +0530

    Redesigning the buttons on the 30 day grid

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,32 +1,68 @@
-import {workoutProgram as training_plan} from '../utils/index.js'
+import { useState } from 'react'
+import { workoutProgram as training_plan } from '../utils/index.js'
+import WorkoutCard from './WorkoutCard.jsx'
 
 export default function Grid() {
-    const isLocked = false 
+    const [selectedWorkout, setSelectedWorkout] = useState(null)
+
+    const isLocked = true
+
     return (
         <div className="training-grid-plan">
             {Object.keys(training_plan).map((workout, workoutIndex) => {
+                const type =
+                    workoutIndex % 3 === 0
+                        ? 'Push'
+                        : workoutIndex % 3 === 1
+                        ? 'Pull'
+                        : 'Legs'
+
+                const trainingPlan = training_plan[workout]
+
+                if (workoutIndex === selectedWorkout) {
+                    return (
+                        <WorkoutCard
+                            key={workoutIndex}
+                            trainingPlan={trainingPlan}
+                        />
+                    )
+                }
+
                 return (
-                    <button key ={workoutIndex}>
-                        <div className = 'plan-card-header'>
-                            <p>Day{(workoutIndex /88 <= 1) ? '0' + (workoutIndex + 1) 
-                            : workoutIndex + 1}</p>
+                    <button
+                        className={`card plan-card ${isLocked ? 'inactive' : ''}`}
+                        key={workoutIndex}
+                        disabled={isLocked}
+                        onClick={() => {
+                            if (!isLocked) {
+                                setSelectedWorkout(workoutIndex)
+                            }
+                        }}
+                    >
+                        <div className="plan-card-header">
+                            <p>
+                                Day {(workoutIndex + 1 < 10)
+                                    ? '0' + (workoutIndex + 1)
+                                    : workoutIndex + 1}
+                            </p>
                         </div>
+
                         {isLocked ? (
-                            <i className='fa-solid fa-lock'></i>
+                            <i className="fa-solid fa-lock"></i>
+                        ) : workoutIndex % 3 === 0 ? (
+                            <i className="fa-solid fa-dumbbell"></i>
+                        ) : workoutIndex % 3 === 1 ? (
+                            <i className="fa-solid fa-weight-hanging"></i>
                         ) : (
-                            workoutIndex % 3 === 0 ? (
-                                <i className ='fa-solid fa-dumbbell'></i>
-                            ) : (
-                                workoutIndex % 3 === 1 ? (
-                                    <i className = 'fa-solid fa-weight-hanging'></i>
-                                ) : (
-                                    <i className = 'fa-solid fa-bolt'></i>
-                                )
-                            )
+                            <i className="fa-solid fa-bolt"></i>
                         )}
+
+                        <div className="plan-card-header">
+                            <h4><b>{type}</b></h4>
+                        </div>
                     </button>
                 )
             })}
         </div>
-    );  
+    )
 }
