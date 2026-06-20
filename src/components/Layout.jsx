cb062c7d1d583728d2529709e89651d095b386c4
Pratyakshya Mishra <pratyakshya.mishra43@gmail.com>
2026-06-20 08:05:40 +0530

    feat: add app layout structure

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,14 +4,14 @@ export default function Layout(props) {
 
     const header = (
         <header>
-            <h1 className="text-gradient">The Brogram</h1>
+            <h1 className="text-gradient">FitForge</h1>
             <p><strong>The 30 simple workouts program</strong></p>
         </header>
     )
 
     const footer = (
         <footer>
-            <p>&copy; {new Date().getFullYear()} The Brogram. All rights reserved.<br/><a href = "https://www.fantacss.smoljames.com"
+            <p>&copy; {new Date().getFullYear()} FitForge. All rights reserved.<br/><a href = "https://www.fantacss.smoljames.com"
              target="_blank">Fantacss</a></p>
                 <p>Built By: Pratyakshya</p>
         </footer>
