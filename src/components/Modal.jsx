9b360579960cea6a5a06ec29d536ae7004ff5490
Pratyakshya Mishra <pratyakshya.mishra43@gmail.com>
2026-06-20 09:05:09 +0530

    feat: add exercise detail modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,25 +1,21 @@
-import ReactDom from 'react-dom'
+export default function Modal(props) {
+    const { exercise, handleCloseModal } = props
 
-export default function Modal(props){
+    return (
+        <div className="modal-container">
+            <button className="modal-underlay" onClick={handleCloseModal}></button>
 
-    const { showExerciseDescription, handleCloseModal } = props
-    const {name, description} = showExerciseDescription || {}
-    
-    return ReactDom.createPortal((
-        <div className = 'modal-container'>
-            <button className='modal-underlay' onClick={handleCloseModal}/>
-            <div className='modal-content'>
+            <div className="modal-content">
                 <div>
-                    <h6>Name</h6>
-                    <h2 className='skill-name'>{name.replaceAll('-','.')}</h2>
+                    <h4>Name</h4>
+                    <h2>{exercise.name}</h2>
                 </div>
+
                 <div>
-                    <h6>Description</h6>
-                    <p>{description}</p>
+                    <h4>Description</h4>
+                    <p>{exercise.description || "No description available."}</p>
                 </div>
             </div>
         </div>
-    ),
-    document.getElementById('portal')
     )
 }
